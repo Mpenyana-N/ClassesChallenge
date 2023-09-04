a3f2e088fe548615170cc6d81b88cc115e07d21d
Nick Mpenyana <nickmpenyana@gmail.com>
2023-09-04 06:50:53 +0200

    challenge done

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,21 +1,25 @@
 public class BankAccount {
-    private int AccountNumber;
-    private int balance;
-    private int phoneNumber;
+    private long AccountNumber;
+    private double balance;
+    private Long phoneNumber;
     private String name;
     private String email;
 
-//    Setters
-    public void setAccountNumber(int accountNumber) {
+    //    Setters
+    public void setAccountNumber(long accountNumber) {
         this.AccountNumber = accountNumber;
     }
-    public void setBalance(int balance) {
+
+    public void setBalance(double balance) {
         this.balance = balance;
+
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public void setPhoneNumber(int phoneNumber) {
+
+    public void setPhoneNumber(Long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
@@ -26,15 +30,15 @@ public class BankAccount {
 //    Getters
 
 
-    public int getAccountNumber() {
+    public long getAccountNumber() {
         return AccountNumber;
     }
 
-    public int getBalance() {
+    public double getBalance() {
         return balance;
     }
 
-    public int getPhoneNumber() {
+    public Long getPhoneNumber() {
         return phoneNumber;
     }
 
@@ -46,6 +50,17 @@ public class BankAccount {
         return name;
     }
 
-    public void depositFunds() {}
-    public void withDrawls(){}
+    public void depositFunds(int funds) {
+       this.balance += funds;
+        System.out.println("Deposited funds = " + funds + " New balance = " + this.balance);
+    }
+
+    public void withDrawls(int withdrawal) {
+        if (balance > 0 && balance > withdrawal) {
+            this.balance -= withdrawal;
+            System.out.println("Withdrawal = " + withdrawal + " New Balance = " + this.balance);
+        } else {
+            System.out.println("Withdrawal = " + withdrawal + " Insufficient funds Withdrawal not processed " + " Remaining balance = " + this.balance);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setBalance(0);
+        bankAccount.setName("Nick");
+        bankAccount.setAccountNumber(9105145369086L);
+        bankAccount.setEmail("trapdilla50@gmail.com");
+        bankAccount.setPhoneNumber(+27_67_756_9716L);
+        bankAccount.depositFunds(10000);
+        bankAccount.withDrawls(6000);
+        bankAccount.withDrawls(60000);
+        bankAccount.depositFunds(300000);
+        bankAccount.withDrawls(60000);
+        System.out.println(bankAccount.getName());
+        System.out.println(bankAccount.getBalance());
+        System.out.println(bankAccount.getPhoneNumber());
+        System.out.println(bankAccount.getEmail());
+        System.out.println(bankAccount.getAccountNumber());
+
 
     }
 }
